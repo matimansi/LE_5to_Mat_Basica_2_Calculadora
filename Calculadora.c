3b456cd70b8398225755c7a9433a76caf4525b55
Matias Mansilla <mmansilla@etrr.edu.ar>
2022-11-10 20:58:12 -0300

    1.0V

--- Calculadora.c
+++ Calculadora.c
@@ -1,12 +1,5 @@
-/*
-	Recibo dos valores y un caracter que indique la operación (+, -, /, *) a realizar.
-	Verificar casos indeterminados. Devuelvo el resultado.
-
-	>>> TERMINADO <<<
-*/
 
 #include <stdio.h>
-//#include <stdio_ext.h>
 
 float calculadora (int valor_a, int valor_b, char op);
 
@@ -15,39 +8,32 @@ int main()
 	signed int num1, num2;
 	char operacion;
 	float resultado;
-	printf("Bienvenido a mi programa...\n");
+	printf("\nBienvenido a mi programa...\n");
 	printf("Ingrese el primer valor entero\n>>> ");
-	__fpurge(stdin);
+	fflush(stdin);
 	scanf("%d", &num1);
 	printf("Ingrese el segundo valor entero\n>>> ");
-	__fpurge(stdin);
+	fflush(stdin);
 	scanf("%d", &num2);
 	printf("Operacion [ + | - | * | / ]\n>>> ");
-	__fpurge(stdin);
+	fflush(stdin);
 	scanf("%c", &operacion);
 	resultado = calculadora(num1, num2, operacion);
-	printf("Resultado: %.3f", resultado);
+	printf("Resultado: %.3f\n\n", resultado);
 	return 0;
 }
 
 float calculadora (int valor_a, int valor_b, char op)
 {
 	float resultado;
-	if (op == '+')
-	{
-		resultado = valor_a + valor_b;
-	}
-	if (op == '-')
-	{
-		resultado = valor_a - valor_b;
-	}
-	if (op == '*')
-	{
-		resultado = valor_a * valor_b;
-	}
+	if (op == '+') resultado = valor_a + valor_b;
+	if (op == '-') resultado = valor_a - valor_b;
+	if (op == '*') resultado = valor_a * valor_b;
 	if (op == '/')
 	{
-		resultado = valor_a / valor_b;
+		if ((valor_a != 0) && (valor_b == 0)) printf("No se puede dividir por 0\n");
+		if ((valor_a == 0) && (valor_b == 0)) printf("Resultado Indefinido\n");
+		if ((valor_a != 0) && (valor_b != 0)) resultado = valor_a / valor_b;
 	}
 	return (resultado);
 }
